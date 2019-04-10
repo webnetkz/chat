fe26d8d3509ad16bc795f746f1c245be25c68562
webnetkz <8gg001@gmail.com>
2019-04-11 00:20:14 +0600

    start new project

--- app/views/account/login.php
+++
@@ -1,7 +0,0 @@
-<h4>Login</h4>
-<hr>
-<form action="/account/login" method="POST">
-	<p><input type="text" name="name"></p>
-	<p><input type="password" name="pass"></p>
-	<button type="send" name="send">Send</button>
-</form>
--- app/views/news/show.php
+++
@@ -1,9 +0,0 @@
-<h2>news</h2>
-<hr>
-<?php
- // Отображение новостей
-foreach($news as $value) {
-	echo '<h4>'. $value['title'] .'</h4>';
-	echo '<p>'. $value['description'] .'</p><hr>';
-}
-?>
